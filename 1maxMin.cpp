1e3642c6be9af72296f6f89955b64cba689807cf
abhishekchauhan15 <76480451+abhishekchauhan15@users.noreply.github.com>
2021-04-27 10:32:37 +0530

    Add files via upload

---
+++ 1maxMin.cpp
@@ -0,0 +1,46 @@
+#include<iostream> 
+using namespace std;
+
+void maxmin(int arr[], int n){
+
+    if(n==1){
+        cout<<"min number is "<<arr[0];
+        cout<<"max number is "<<arr[0];
+    }      
+    
+                                        //In order to minimize the number of comparisions we are increasing the no. of cases
+    int max=INT_MIN;
+    int min=INT_MAX;
+
+    for(int i=0; i<n; i++){
+        if(arr[i]<min){                                     //no f comp can be more reduced 
+            min=arr[i];                                     // min no of comparision is 2*n 
+        }
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+
+    cout<<"max element is "<<max<<"\n";
+    cout<<"min element is "<<min;
+
+
+    
+
+
+}
+
+int main(){
+
+int n;
+cout<<"enter the size";
+cin>>n;
+int arr[n];
+for(int i=0; i<n; i++)
+   cin>>arr[i];
+
+maxmin(arr, n);
+
+
+return 0;
+}
