b6a018f30421d6f08ee9e8db0c8577f662954adc
abhishekchauhan15 <76480451+abhishekchauhan15@users.noreply.github.com>
2021-06-07 16:35:47 +0530

    Update 4negativeNumbersToBeginning.cpp

--- 4negativeNumbersToBeginning.cpp
+++ 4negativeNumbersToBeginning.cpp
@@ -1,10 +1,8 @@
-#include<iostream> 
-using namespace std;
 void rearr(int arr[], int n){
     int low=0;
     int high =n-1;
 
-    while(high>=low){                                              // o(1)
+    while(high>=low){                                             // Time Complexity: o(n)
         if(arr[low]<0 && arr[high]<0){
                                                         //--
             low++;
@@ -23,24 +21,4 @@ void rearr(int arr[], int n){
             high--;
         }
     }
-}
-int main(){
-int n;
-cout<<"enter ";
-
-    cin>>n;
-
-    int arr[n];
-for(int i=0; i<n; i++)
-cin>>arr[i];
-
-rearr(arr, n);
-
-for(int i=0; i<n; i++)
-cout<<arr[i]<<" ";
-
-
-    
-
-return 0;
 }
