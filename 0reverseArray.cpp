f0085c20ca690774c11daa02ac47252d47b6e54d
abhishekchauhan15 <76480451+abhishekchauhan15@users.noreply.github.com>
2021-06-07 12:08:02 +0530

    Update 0reverseArray.cpp

--- 0reverseArray.cpp
+++ 0reverseArray.cpp
@@ -1,55 +1,23 @@
-#include<iostream> 
-using namespace std;
-void reverse(int arr[], int n){
+#Methode 1
+int reverse(int arr[], int n){
     int start=0, end=n-1;
-    while(start<end){                       // 9, 8 , 7, 6,43
-        int temp= arr[start];
+    while(start<end){                    
+        int temp= arr[start];               // time complexicity is o (n) 
         arr[start]=arr[end];
         arr[end]=temp;
         start++;
         end--;
-
-}
-}                                         // time complexicity is o (1) 
-
-int main(){
-int n;
-cout<<"enter the size";
-cin>>n;
-int arr[n];
-for(int i=0; i<n; i++)
-cin>>arr[i];
-
-reverse(arr, n);
-for(int i=0; i<n; i++)
-cout<<arr[i]<<" ";
-
-return 0;
-}
-
-
-// #include<iostream> 
-// #include<bits.h>
-
-// using namespace std;
-// void reverse(int arr[], int n){
-//     for(int i=0; i=n/2; i++){
-//         swap(arr[i], arr[n-1-i]);                        // time complexicity is o (N) 
-//     }
+    }
+    return arr;
     
-// }
-// int main(){
+}                                         
 
-// int n;
-// cout<<"enter the size";
-// cin>>n;
-// int arr[n];
-// for(int i=0; i<n; i++)
-// cin>>arr[i];
 
-// reverse(arr, n);
-// for(int i=0; i<n; i++)
-// cout<<arr[i]<<" ";
 
-// return 0;
-// }
+#Methode 2
+void reverse(int arr[], int n){
+    for(int i=0; i=n/2; i++){
+        swap(arr[i], arr[n-1-i]);                        // time complexicity is o (n) 
+    }
+    
+}
