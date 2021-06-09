7f8a1acce23025ae76401e375098d706d1ae8fd1
abhishekchauhan15 <76480451+abhishekchauhan15@users.noreply.github.com>
2021-06-09 11:09:58 +0530

    Create 6)CyclicallyRotate.cpp

---
+++ Arrays/6)CyclicallyRotate.cpp
@@ -0,0 +1,13 @@
+void ro(int arr[], int n)
+{
+     int temp=arr[n-1];
+   for(int i=n-1; i>=0; i--)                    Time Complexity:o(n)
+      arr[i]=arr[i-1];
+      
+      arr[0]=temp;
+
+      for(int i=0; i<n; i++){
+          cout<<arr[i]<<" ";
+      }
+    
+}
